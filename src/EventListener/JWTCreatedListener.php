5221fd3591b77345c4c994cbb95d07cfdc817810
jonas <jonasxtrm971@gmail.com>
2020-08-06 16:17:50 +0200

    ajout de la date dans le token grace aux events, permettre la génération du token via /api/login_check et possibilité d'autoriser le token grace au bouton authorize, tous ça dans le SwaggerUI de api-plateForme

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class JWTCreatedListener
+{
+    /** @var UserInterface $user  */
+    private UserInterface $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
+
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        // dd($payload);
+        $payload['createdAt'] = $this->user->getCreatedAt();
+        $event->setData($payload);
+    }
+}
