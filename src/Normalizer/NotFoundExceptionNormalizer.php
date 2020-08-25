8fb2991ce03e5220e0ef2f760d8918f77e68b2fe
jonas <jonasxtrm971@gmail.com>
2020-08-25 16:15:31 +0200

    refacto dans le dossier Factory et creation de src/Normalizer/NotFoundExceptionNormalizer.php

---
+++ src/Normalizer/NotFoundExceptionNormalizer.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Normalizer;
+
+use App\Normalizer\AbstractNormalizer;
+use Symfony\Component\HttpFoundation\Response;
+
+class NotFoundExceptionNormalizer extends AbstractNormalizer
+{
+    public function normalize(\Exception $exception) : array
+    {
+        return $this->exceptionNormalizerFormatter->format(
+           $exception->getMessage(),
+           Response::HTTP_NOT_FOUND
+        );
+    }
+}
